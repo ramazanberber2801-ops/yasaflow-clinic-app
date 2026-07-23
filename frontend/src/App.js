919f37671b47206368c111a5b85aceb1666af76b
ramazanberber2801-ops <ramazanberber2801@gmail.com>
2026-07-23 09:52:52 +0300

    Route CRM customer list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import Admin from "@/pages/AdminHub";
 import AdminOverview from "@/pages/AdminOverview";
 import AdminSubscription from "@/pages/AdminSubscription";
 import AdminBirthdayAutomation from "@/pages/AdminBirthdayAutomation";
+import AdminCustomers from "@/pages/AdminCustomers";
 import AdminSettings from "@/pages/AdminSettings";
 import AdminNotifications from "@/pages/AdminNotifications";
 import AdminLoyaltyCampaigns from "@/pages/AdminLoyaltyCampaigns";
@@ -55,6 +56,7 @@ function App() {
           <Route path="/admin/overview" element={<ProtectedAdminRoute><AdminOverview /></ProtectedAdminRoute>} />
           <Route path="/admin/subscription" element={<ProtectedAdminRoute><AdminSubscription /></ProtectedAdminRoute>} />
           <Route path="/admin/birthdays" element={<ProtectedAdminRoute><AdminBirthdayAutomation /></ProtectedAdminRoute>} />
+          <Route path="/admin/customers" element={<ProtectedAdminRoute><AdminCustomers /></ProtectedAdminRoute>} />
           <Route path="/admin/settings" element={<ProtectedAdminRoute><AdminSettings /></ProtectedAdminRoute>} />
           <Route path="/admin/notifications" element={<ProtectedAdminRoute><AdminNotifications /></ProtectedAdminRoute>} />
           <Route path="/admin/loyalty-campaigns" element={<ProtectedAdminRoute><AdminLoyaltyCampaigns /></ProtectedAdminRoute>} />
